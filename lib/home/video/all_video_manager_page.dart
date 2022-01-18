0d9853908cc21beb3938e230457c3846a423db8c
欧阳锋 <ouyangfeng2016@gmail.com>
2022-01-18 19:44:06 +0800

    视频列表页面：新增右键菜单以及相关操作

--- lib/home/video/all_video_manager_page.dart
+++ lib/home/video/all_video_manager_page.dart
@@ -1,9 +1,15 @@
 import 'dart:convert';
 import 'dart:developer' as developer;
 import 'dart:async';
+import 'dart:io';
+import 'dart:ui';
 
+import 'package:file_picker/file_picker.dart';
+import 'package:flowder/flowder.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:mobile_assistant_client/event/update_delete_btn_status.dart';
 import 'package:mobile_assistant_client/event/update_video_sort_order.dart';
 import 'package:mobile_assistant_client/home/video_manager_page.dart';
@@ -11,6 +17,7 @@ import 'package:mobile_assistant_client/model/video_item.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:mobile_assistant_client/network/device_connection_manager.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile_assistant_client/widget/progress_indictor_dialog.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 import '../../model/ResponseEntity.dart';
@@ -38,6 +45,10 @@ class _AllVideoManagerState extends State<AllVideoManagerPage> with AutomaticKee
   final _IMAGE_GRID_BORDER_WIDTH_SELECTED = 4.0;
   final _IMAGE_GRID_BORDER_WIDTH = 1.0;
 
+  final _KB_BOUND = 1 * 1024;
+  final _MB_BOUND = 1 * 1024 * 1024;
+  final _GB_BOUND = 1 * 1024 * 1024 * 1024;
+
   List<VideoItem> _selectedVideos = [];
   final _URL_SERVER = "http://${DeviceConnectionManager.instance.currentDevice?.ip}:8080";
 
@@ -49,6 +60,9 @@ class _AllVideoManagerState extends State<AllVideoManagerPage> with AutomaticKee
   late Function() _ctrlAPressedCallback;
 
   bool _isPageVisible = false;
+
+  DownloaderCore? _downloaderCore;
+  ProgressIndicatorDialog? _progressIndicatorDialog;
 
   @override
   void initState() {
@@ -298,6 +312,11 @@ class _AllVideoManagerState extends State<AllVideoManagerPage> with AutomaticKee
     }
   }
 
+  bool _isMouseRightClicked(PointerDownEvent event) {
+    return event.kind == PointerDeviceKind.mouse &&
+        event.buttons == kSecondaryMouseButton;
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = Container(
@@ -311,79 +330,90 @@ class _AllVideoManagerState extends State<AllVideoManagerPage> with AutomaticKee
           VideoItem videoItem = _videos[index];
           String duration = _convertToReadableTime(videoItem.duration);
 
-          return Stack(
-            children: [
-              Container(
-                child: GestureDetector(
-                  child: CachedNetworkImage(
-                    imageUrl:
-                    "http://${DeviceConnectionManager.instance.currentDevice?.ip}:8080/stream/video/thumbnail/${videoItem.id}/200/200",
-                    fit: BoxFit.cover,
-                    width: 160,
-                    height: 160,
-                    memCacheWidth: 400,
-                    fadeOutDuration: Duration.zero,
-                    fadeInDuration: Duration.zero,
-                    errorWidget: (context, url, _) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(
+          return Listener(
+            child: Stack(
+              children: [
+                Container(
+                  child: GestureDetector(
+                    child: CachedNetworkImage(
+                      imageUrl:
+                      "http://${DeviceConnectionManager.instance.currentDevice?.ip}:8080/stream/video/thumbnail/${videoItem.id}/200/200",
+                      fit: BoxFit.cover,
+                      width: 160,
+                      height: 160,
+                      memCacheWidth: 400,
+                      fadeOutDuration: Duration.zero,
+                      fadeInDuration: Duration.zero,
+                      errorWidget: (context, url, _) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
                               color: Color(0xffe1e1e1),
+                            ),
+                            color: Color(0xfff9f9f9),
                           ),
-                          color: Color(0xfff9f9f9),
-                        ),
-                      );
+                        );
+                      },
+                    ),
+                    onTap: () {
+                      _setVideoSelected(videoItem);
+                    },
+                    onDoubleTap: () {
+                      debugPrint("双击");
+                      _openVideoWithSystemApp(videoItem);
                     },
                   ),
-                  onTap: () {
-                    _setVideoSelected(videoItem);
-                  },
-                  onDoubleTap: () {
-                    debugPrint("双击");
-                    _openVideoWithSystemApp(videoItem);
-                  },
+                  decoration: BoxDecoration(
+                      border: new Border.all(
+                          color: _isContainsVideo(_selectedVideos, videoItem)
+                              ? Color(0xff5d86ec)
+                              : Color(0xffdedede),
+                          width: _isContainsVideo(_selectedVideos, videoItem)
+                              ? _IMAGE_GRID_BORDER_WIDTH_SELECTED
+                              : _IMAGE_GRID_BORDER_WIDTH),
+                      borderRadius: new BorderRadius.all(Radius.circular(
+                          _isContainsVideo(_selectedVideos, videoItem)
+                              ? _IMAGE_GRID_RADIUS_SELECTED
+                              : _IMAGE_GRID_RADIUS))),
                 ),
-                decoration: BoxDecoration(
-                    border: new Border.all(
-                        color: _isContainsVideo(_selectedVideos, videoItem)
-                            ? Color(0xff5d86ec)
-                            : Color(0xffdedede),
-                        width: _isContainsVideo(_selectedVideos, videoItem)
-                            ? _IMAGE_GRID_BORDER_WIDTH_SELECTED
-                            : _IMAGE_GRID_BORDER_WIDTH),
-                    borderRadius: new BorderRadius.all(Radius.circular(
-                        _isContainsVideo(_selectedVideos, videoItem)
-                            ? _IMAGE_GRID_RADIUS_SELECTED
-                            : _IMAGE_GRID_RADIUS))),
-              ),
-              Container(
-                child: Align(
-                  child: Row(
-                    children: [
-                      Image.asset("icons/ic_video_indictor.png", width: 20, height: 20),
+                Container(
+                  child: Align(
+                    child: Row(
+                      children: [
+                        Image.asset("icons/ic_video_indictor.png", width: 20, height: 20),
 
-                      Container(
-                        child: Text(duration, style: TextStyle(
-                            inherit: false,
-                            fontSize: 14,
-                            color: Colors.white,
-                          shadows: [
-                            Shadow(
-                              offset: Offset(0, 0),
-                              blurRadius: 3.0,
-                              color: Colors.black
-                            )
-                          ]
-                        )),
-                        margin: EdgeInsets.only(left: 5),
-                      )
-                    ],
+                        Container(
+                          child: Text(duration, style: TextStyle(
+                              inherit: false,
+                              fontSize: 14,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                    offset: Offset(0, 0),
+                                    blurRadius: 3.0,
+                                    color: Colors.black
+                                )
+                              ]
+                          )),
+                          margin: EdgeInsets.only(left: 5),
+                        )
+                      ],
+                    ),
+                    alignment: Alignment.bottomLeft,
                   ),
-                  alignment: Alignment.bottomLeft,
-                ),
-                margin: EdgeInsets.only(left: 10, bottom: 10),
-              )
-            ],
+                  margin: EdgeInsets.only(left: 10, bottom: 10),
+                )
+              ],
+            ),
+            onPointerDown: (event) {
+              if (_isMouseRightClicked(event)) {
+                _openMenu(event.position, videoItem);
+
+                if (!_isSelected(videoItem)) {
+                  _setVideoSelected(videoItem);
+                }
+              }
+            },
           );
         },
         itemCount: _videos.length,
@@ -410,6 +440,133 @@ class _AllVideoManagerState extends State<AllVideoManagerPage> with AutomaticKee
         });
   }
 
+  bool _isSelected(VideoItem videoItem) {
+    return _selectedVideos.contains(videoItem);
+  }
+
+  void _openMenu(Offset position, VideoItem videoItem) {
+    RenderBox? overlay =
+    Overlay.of(context)?.context.findRenderObject() as RenderBox;
+
+    String name = videoItem.name;
+
+    showMenu(
+        context: context,
+        position: RelativeRect.fromSize(
+            Rect.fromLTRB(position.dx, position.dy, 0, 0),
+            overlay.size ?? Size(0, 0)),
+        items: [
+          PopupMenuItem(
+              child: Text("打开"),
+              onTap: () {
+                _openVideoWithSystemApp(videoItem);
+              }),
+          PopupMenuItem(
+              child: Text("拷贝$name到电脑"),
+              onTap: () {
+                _openFilePicker(videoItem);
+              }),
+          PopupMenuItem(
+              child: Text("删除"),
+              onTap: () {
+                // Future<void>.delayed(
+                //     const Duration(),
+                //         () => _tryToDeleteFiles(
+                //         AllFileManager.instance.selectedFiles()));
+              }),
+        ]);
+  }
+
+  void _showDownloadProgressDialog(VideoItem videoItem) {
+    if (null == _progressIndicatorDialog) {
+      _progressIndicatorDialog = ProgressIndicatorDialog(context: context);
+      _progressIndicatorDialog?.onCancelClick(() {
+        _downloaderCore?.cancel();
+        _progressIndicatorDialog?.dismiss();
+      });
+    }
+
+    String title = "正在准备中，请稍后...";
+    _progressIndicatorDialog?.title = title;
+
+    if (!_progressIndicatorDialog!.isShowing) {
+      _progressIndicatorDialog!.show();
+    }
+  }
+  
+  void _openFilePicker(VideoItem videoItem) async {
+    String? dir = await FilePicker.platform
+        .getDirectoryPath(dialogTitle: "选择目录", lockParentWindow: true);
+
+    if (null != dir) {
+      debugPrint("Select directory: $dir");
+
+      _showDownloadProgressDialog(videoItem);
+
+      _downloadFile(videoItem, dir, () {
+        _progressIndicatorDialog?.dismiss();
+      }, (error) {
+        SmartDialog.showToast(error);
+      }, (current, total) {
+        if (_progressIndicatorDialog?.isShowing == true) {
+          if (current > 0) {
+            setState(() {
+              _progressIndicatorDialog?.title = "正在导出视频 ${videoItem.name}";
+            });
+          }
+
+          setState(() {
+            _progressIndicatorDialog?.subtitle =
+            "${_convertToReadableSize(current)}/${_convertToReadableSize(total)}";
+            _progressIndicatorDialog?.updateProgress(current / total);
+          });
+        }
+      });
+    }
+  }
+
+  String _convertToReadableSize(int size) {
+    if (size < _KB_BOUND) {
+      return "${size} bytes";
+    }
+    if (size >= _KB_BOUND && size < _MB_BOUND) {
+      return "${(size / 1024).toStringAsFixed(1)} KB";
+    }
+
+    if (size >= _MB_BOUND && size <= _GB_BOUND) {
+      return "${(size / 1024 / 1024).toStringAsFixed(1)} MB";
+    }
+
+    return "${(size / 1024 / 1024 / 1024).toStringAsFixed(1)} GB";
+  }
+
+  void _downloadFile(VideoItem videoItem, String dir, void onSuccess(),
+      void onError(String error), void onDownload(current, total)) async {
+    String name = videoItem.name;
+
+    var options = DownloaderUtils(
+        progress: ProgressImplementation(),
+        file: File("$dir/$name"),
+        onDone: () {
+          debugPrint("Download ${videoItem.name} done");
+          onSuccess.call();
+        },
+        progressCallback: (current, total) {
+          debugPrint("total: $total");
+          debugPrint(
+              "Downloading ${videoItem.name}, percent: ${current / total}");
+          onDownload.call(current, total);
+        });
+
+    String api = "${_URL_SERVER}/stream/file?path=${videoItem.path}";
+
+    if (null == _downloaderCore) {
+      _downloaderCore = await Flowder.download(api, options);
+    } else {
+      _downloaderCore?.download(api, options);
+    }
+  }
+  
   // 转换为更可读时间，单位：s
   String _convertToReadableTime(int duration) {
     double secDur = duration / 1000;
