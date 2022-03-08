433d86c73bc49b38439c41565a9b4d7f33a27dfd
欧阳锋 <ouyangfeng2016@gmail.com>
2022-03-08 16:07:59 +0800

    Delete redundant file "home_cubit.dart".

--- lib/home2/cubit/home_cubit.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:mobile_assistant_client/home2/bloc/home_state.dart';
-//
-// class HomeCubit extends Cubit<HomeState> {
-//   HomeCubit() : super(const HomeState()) {
-//   }
-//
-//   void setTab(HomeTab tab) => emit(HomeState(tab: tab));
-// }
