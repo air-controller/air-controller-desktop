ac14537806be2995abc5ad6605ef914c5c077aed
欧阳锋 <ouyangfeng2016@gmail.com>
2022-03-31 11:38:41 +0800

    Set the window title to "AirController" on Linux.

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -44,7 +44,7 @@ static void my_application_activate(GApplication* application) {
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "air_controller_desktop");
+    gtk_window_set_title(window, "AirController");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
