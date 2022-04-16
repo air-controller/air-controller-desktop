a5ccb19dab931a599092d9fb436173b749ca40b7
ouyangfeng <ouyangfeng2016@gmail.com>
2022-04-16 02:05:06 -0700

    Hide title bar only on Linux platform.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -10,9 +12,12 @@ void main() async {
   await windowManager.ensureInitialized();
 
   windowManager.waitUntilReadyToShow().then((_) async {
-    await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
-    await windowManager.setMinimumSize(Size(Constant.MIN_WINDOW_WIDTH, Constant.MIN_WINDOW_HEIGHT));
-    await windowManager.setSize(Size(Constant.DEFAULT_WINDOW_WIDTH, Constant.DEFAULT_WINDOW_HEIGHT));
+    await windowManager.setTitleBarStyle(
+        Platform.isMacOS ? TitleBarStyle.hidden : TitleBarStyle.normal);
+    await windowManager.setMinimumSize(
+        Size(Constant.MIN_WINDOW_WIDTH, Constant.MIN_WINDOW_HEIGHT));
+    await windowManager.setSize(
+        Size(Constant.DEFAULT_WINDOW_WIDTH, Constant.DEFAULT_WINDOW_HEIGHT));
     await windowManager.center();
     await windowManager.setHasShadow(true);
     await windowManager.setBrightness(Brightness.light);
